192767ee6f51b7bfbbfda887451d70641aeb4d70
WeDoBoring <wedoboring@gmail.com>
2026-01-23 06:34:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7450855371-71d00",
+  "appId": "1:196251746391:web:0a17e9c032499a358c2312",
+  "apiKey": "AIzaSyBbPv726fWn5DDdYsQbXO32imuQfBp1Y1U",
+  "authDomain": "studio-7450855371-71d00.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "196251746391"
+};
