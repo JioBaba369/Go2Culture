72ed575c10bd62f8030e675e7e9a3b930a091180
WeDoBoring <wedoboring@gmail.com>
2026-01-25 01:49:12 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://www.col

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.coles.com.au',
       }
     ],
   },
