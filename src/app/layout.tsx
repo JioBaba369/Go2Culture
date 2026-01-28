6957d8f847964de8d9cdbd3c36a641f081ea5495
WeDoBoring <wedoboring@gmail.com>
2026-01-28 12:12:01 +0000

    also, add Dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className={cn('antialiased font-sans', lora.variable, inter.variable)}>
         <FirebaseClientProvider>
           <MainLayout header={<Header />} footer={<Footer />}>
